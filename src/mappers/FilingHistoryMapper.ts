f91c2366580a5ab4bc6c670a6e49ef784c9ad053
kinpang-CH <kpang@companieshouse.gov.uk>
2022-09-08 12:36:30 +0100

    Refactor mapFilingHistoryDate method
    * Take boolean parameter to determine full month
    * Change respective methods in classes

--- src/mappers/FilingHistoryMapper.ts
+++ src/mappers/FilingHistoryMapper.ts
@@ -1,19 +1,12 @@
 import fs from "fs";
 import yaml from "js-yaml";
+import dayjs from "dayjs";
 
 const FILING_HISTORY_DESCRIPTIONS_PATH: string = "api-enumerations/filing_history_descriptions.yml";
 const DESCRIPTIONS_CONSTANT: string = "description";
 
 const filingHistoryDescriptions: any = yaml.safeLoad(fs.readFileSync(FILING_HISTORY_DESCRIPTIONS_PATH, "utf8"));
 
-export const mapFilingHistoryDate = (dateString: string): string => {
-    const d = new Date(dateString);
-    const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
-    const month = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
-    const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
-
-    return `${day} ${month} ${year}`;
-};
 
 export const mapFilingHistory = (description: string, descriptionValues: Record<string, string>): string => {
     const descriptionFromFile = getFullFilingHistoryDescription(description);
@@ -26,19 +19,16 @@ export const mapFilingHistoryDescriptionValues = (description: string, descripti
         return descriptionValues.description;
     } else {
         return Object.entries(descriptionValues).reduce((newObj, [key, val]) => {
-            const value = key.includes("date") ? mapDateFullMonth(val) : val;
+            const value = key.includes("date") ? mapFilingHistoryDate(val, true) : val;
             return newObj.replace("{" + key + "}", value as string);
         }, description);
     }
 };
 
-export const mapDateFullMonth = (dateString: string): string => {
-    const d = new Date(dateString);
-    const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
-    const month = new Intl.DateTimeFormat("en", { month: "long" }).format(d);
-    const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
-
-    return `${day} ${month} ${year}`;
+export const mapFilingHistoryDate = (dateString: string, fullMonth: boolean): string => {
+    const date = Date.parse(dateString);
+    return fullMonth == true ? dayjs(date).format("DD MMMM YYYY") :
+        dayjs(date).format("DD MMM YYYY");
 };
 
 export const removeAsterisks = (description: string) => {
