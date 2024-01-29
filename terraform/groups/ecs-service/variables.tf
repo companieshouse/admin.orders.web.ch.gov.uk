862bb91570b0a262c75b1d7d2c94d5e9eb4b9d76
Ivana Pavic <ipavic@companieshouse.gov.uk>
2024-01-29 15:39:22 +0000

    Updating infra for node 20

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -42,99 +42,93 @@ variable "required_memory" {
   description = "The required memory for this service"
   default     = 256 # defaulted low for node service in dev environments, override for production
 }
+variable "eric_cpus" {
+  type = number
+  description = "The required cpu resource for eric. 1024 here is 1 vCPU"
+  default = 256
+}
+variable "eric_memory" {
+  type = number
+  description = "The required memory for eric"
+  default = 512
+}
+variable "max_task_count" {
+  type        = number
+  description = "The maximum number of tasks for this service."
+  default     = 3
+}
+
 variable "use_fargate" {
   type        = bool
   description = "If true, sets the required capabilities for all containers in the task definition to use FARGATE, false uses EC2"
-  default     = false
+  default     = true
 }
+variable "use_capacity_provider" {
+  type        = bool
+  description = "Whether to use a capacity provider instead of setting a launch type for the service"
+  default     = true
+}
+variable "service_autoscale_enabled" {
+  type        = bool
+  description = "Whether to enable service autoscaling, including scheduled autoscaling"
+  default     = true
+}
+variable "service_autoscale_target_value_cpu" {
+  type        = number
+  description = "Target CPU percentage for the ECS Service to autoscale on"
+  default     = 50 # 100 disables autoscaling using CPU as a metric
+}
+variable "service_scaledown_schedule" {
+  type        = string
+  description = "The schedule to use when scaling down the number of tasks to zero."
+  # Typically used to stop all tasks in a service to save resource costs overnight.
+  # E.g. a value of '55 19 * * ? *' would be Mon-Sun 7:55pm.  An empty string indicates that no schedule should be created.
+
+  default     = ""
+}
+variable "service_scaleup_schedule" {
+  type        = string
+  description = "The schedule to use when scaling up the number of tasks to their normal desired level."
+  # Typically used to start all tasks in a service after it has been shutdown overnight.
+  # E.g. a value of '5 6 * * ? *' would be Mon-Sun 6:05am.  An empty string indicates that no schedule should be created.
+
+  default     = ""
+}
+
+# ----------------------------------------------------------------------
+# Cloudwatch alerts
+# ----------------------------------------------------------------------
+variable "cloudwatch_alarms_enabled" {
+  description = "Whether to create a standard set of cloudwatch alarms for the service.  Requires an SNS topic to have already been created for the stack."
+  type        = bool
+  default     = true
+}
+
 # ------------------------------------------------------------------------------
 # Service environment variable configs
 # ------------------------------------------------------------------------------
+variable "ssm_version_prefix" {
+  type        = string
+  description = "String to use as a prefix to the names of the variables containing variables and secrets version."
+  default     = "SSM_VERSION_"
+}
+
+variable "use_set_environment_files" {
+  type        = bool
+  default     = true
+  description = "Toggle default global and shared  environment files"
+}
 variable "log_level" {
   default     = "info"
   type        = string
   description = "The log level for services to use: trace, debug, info or error"
 }
-
 variable "admin_orders_web_version" {
   type        = string
-  description = "The version of the admin orders web container to run."
+  description = "The version of the admin.orders.web.ch.gov.uk container to run."
 }
 
-variable "chs_url" {
+variable "eric_version" {
   type        = string
-}
-variable "cdn_host" {
-  type        = string
-}
-
-variable "piwik_url" {
-  type        = string
-}
-variable "piwik_site_id" {
-  type        = string
-}
-variable "redirect_uri" {
-  type        = string
-  default     = "/"
-}
-variable "cache_pool_size" {
-  type        = string
-  default     = "8"
-}
-
-variable "cookie_domain" {
-  type        = string
-}
-variable "cookie_name" {
-  type        = string
-  default     = "__SID"
-}
-variable "cookie_secure_only" {
-  type        = string
-  default     = "0"
-}
-variable "default_session_expiration" {
-  type        = string
-  default     = "3600"
-}
-
-variable "human_log" {
-  type        = string
-  default     = "1"
-}
-
-variable "radio_button_value_log_length" {
-  type        = string
-  default     = "50"
-}
-variable "show_service_offline_page" {
-  type        = string
-  default     = "false"
-}
-variable "url_log_max_length" {
-  type        = string
-  default     = "400"
-}
-variable "url_param_max_length" {
-  type        = string
-  default     = "50"
-}
-variable "port" {
-  type        = string
-}
-variable "node_env" {
-  type        = string
-}
-variable "orders_search_multibasket_enabled" {
-  type        = string
-}
-variable "api_url" {
-  type        = string
-}
-variable "dispatch_days" {
-  type        = string
-}
-variable "tz" {
-  type        = string
+  description = "The version of the eric container to run."
 }
