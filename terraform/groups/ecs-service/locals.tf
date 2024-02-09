4bba591805ae4f2a42d8be558370ad3ff985cde7
Ivana Pavic <ipavic@companieshouse.gov.uk>
2024-02-09 10:29:11 +0000

    Updating health-check

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -9,8 +9,8 @@ locals {
   kms_alias                  = "alias/${var.aws_profile}/environment-services-kms"
   lb_listener_rule_priority  = 9
   lb_listener_paths          = ["/orders-admin/*"]
-  healthcheck_path           = "/orders-admin/search"
-  healthcheck_matcher        = "302"
+  healthcheck_path           = "/orders-admin/stylesheets/app.css"
+  healthcheck_matcher        = "200"
   vpc_name                   = local.stack_secrets["vpc_name"]
   s3_config_bucket           = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
   app_environment_filename   = "admin.orders.web.ch.gov.uk.env"
